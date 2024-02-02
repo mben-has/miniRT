f4b6ab99f5f7c9cfff41229e56a62057f8b682da
mben-has <mben-has@student.42heilbronn.de>
2024-02-02 14:23:13 +0100

    garbage_collector_update

--- include/Minirt.h
+++ include/Minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marschul <marschul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mben-has <mben-has@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 10:40:38 by marschul          #+#    #+#             */
-/*   Updated: 2024/02/01 21:39:59 by marschul         ###   ########.fr       */
+/*   Updated: 2024/02/02 14:22:51 by mben-has         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "libft.h"
 # include "MLX42.h"
+# include "../src/garbage_collector/garbage_collector.h"
 # include <stdlib.h>
 # include <stdio.h>
 # include <math.h>
